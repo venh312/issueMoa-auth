09f681174a1ae884a9668a5e5d1c0a0583542b60
venh <conf312@naver.com>
2024-05-25 21:55:58 +0900

    [+] (IssueMoa) initialize auth service

---
+++ src/main/kotlin/com/issuemoa/auth/IssueMoaAuthApplication.kt
@@ -0,0 +1,11 @@
+package com.issuemoa.auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IssueMoaAuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<IssueMoaAuthApplication>(*args)
+}
